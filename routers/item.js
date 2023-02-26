8a62f89d54044b95a0f8835b0294db2ff7a3a54e
anas029 <anas029>
2023-02-26 19:46:03 +0300

    server.js edit require routers

--- routers/item.js
+++ routers/item.js
@@ -1,15 +1,15 @@
 const express = require('express')
 
 const router = express.Router()
-router.use(express.urlencoded({extended:true}))
+router.use(express.urlencoded({ extended: true }))
 
 const itemCntrl = require('../controllers/item')
 
 
 //Call API
-router.get('/item/add',itemCntrl.item_create_get)
-router.post('/item/add',itemCntrl.item_create_post)
-router.get('/item/index',itemCntrl.item_index_get)
+router.get('/add', itemCntrl.item_create_get)
+router.post('/add', itemCntrl.item_create_post)
+router.get('/index', itemCntrl.item_index_get)
 
 //exports:
-module.exports=router;
+module.exports = router;
--- routers/user.js
+++ routers/user.js
@@ -5,14 +5,14 @@ const userCntrl = require('../controllers/user')
 
 
 //CALL API:
-router.get('/user/list',userCntrl.user_profile_get)
-router.get('/user/detail',userCntrl.user_detail_get)
-router.get('/user/myProfile',userCntrl.user_currentDetail_get)
-router.get("/user/edit",userCntrl.user_edit_get);
-router.post("/user/edit",userCntrl.user_edit_post);
-// router.get('/user/profile/:id',userCntrl.user_profile_get)
+router.get('/list', userCntrl.user_profile_get)
+router.get('/detail', userCntrl.user_detail_get)
+router.get('/myProfile', userCntrl.user_currentDetail_get)
+router.get("/edit", userCntrl.user_edit_get);
+router.post("/edit", userCntrl.user_edit_post);
+// router.get('/profile/:id',userCntrl.user_profile_get)
 
 
 
 //exports:
-module.exports=router;
+module.exports = router;
