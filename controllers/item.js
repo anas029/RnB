4bc4d47a83a4d826deec2db09cd9edc9477a3a55
Ali Nasere <ali.yaqoob@gmail.com>
2023-02-27 09:02:21 +0300

    add borrow,return,deposit credit ,review

--- controllers/item.js
+++ controllers/item.js
@@ -1,6 +1,7 @@
 // Model
 const Item = require("../models/Item")
 const User = require("../models/User")
+const Review = require("../models/Review")
 
 // HTTP GET - Load item Form
 exports.item_create_get = (req, res, next) => {
@@ -43,4 +44,85 @@ exports.item_details_get = (req, res) => {
             res.render("item/details", { item }))
         .catch(err => console.log(err))
 }
+exports.item_borrow_get = (req, res) => {
+    Item.findById(req.query.id).populate('owner').populate('borrower')
+        .then(item => {
+            if (item.isAvailable)
+                res.render("item/borrowItem", { item, user: req.user })
+        })
+        .catch(err => console.log(err))
+}
+exports.item_borrow_post = (req, res) => {
+    Item.findById(req.query.id)
+        .then(item => {
+            if (item.isAvailable) {
+                User.findById(req.user._id)
+                    .then(user => {
+                        user.credit -= item.dopiste
+                        user.save()
+                    })
+                item.isAvailable = false
+                item.borrower = req.user._id
+                item.borrowDate = Date.now()
+                item.save()
+                res.render("item/borrowItem", { item, user: req.user })
+            }
+        })
+        .catch(err => console.log(err))
+}
+exports.item_return_get = (req, res) => {
+    Item.findById(req.query.id).populate('owner').populate('borrower')
+        .then(item => {
+            if (!item.isAvailable)
+                res.render("item/returnItem", { item, user: req.user })
+        })
+        .catch(err => console.log(err))
+}
+exports.item_return_post = (req, res) => {
+    Item.findById(req.query.id)
+        .then(item => {
+            if (!item.isAvailable) {
+                const balance = item.dopiste - (
+                    item.priceRate * Math.ceil((Date.now() - item.borrowDate) / (1000 * 60 * 60 * 24)))
+                User.findById(item.borrower)
+                    .then(user => {
+                        user.credit += balance
+                        user.save()
+                    })
+                    .catch(e => console.log(e.message))
+                item.isAvailable = true
+                item.borrower = null
+                item.borrowDate = null
+                item.save()
+                const data = req.body
+                data.score = parseInt(req.body.score)
+                data.creatorId = req.user._id
+                data.item = item._id
+                const review = new Review(data)
+                review.save()
+                    .then(res.redirect("/"))
+                    .catch(err => console.log(err))
+            }
+        })
+        .catch(err => console.log(err))
+}
 
+/*
+const Character = mongoose.model('Character', new mongoose.Schema({
+  name: String,
+  age: Number
+}));
+
+await Character.create({ name: 'Jean-Luc Picard' });
+
+const filter = { name: 'Jean-Luc Picard' };
+const update = { age: 59 };
+
+// `doc` is the document _before_ `update` was applied
+let doc = await Character.findOneAndUpdate(filter, update);
+doc.name; // 'Jean-Luc Picard'
+doc.age; // undefined
+
+doc = await Character.findOne(filter);
+doc.age; // 59
+*/
--- models/Item.js
+++ models/Item.js
@@ -4,6 +4,7 @@ const itemSchema = mongoose.Schema({
     itemName: { type: String, required: true },
     description: { type: String, required: true },
     priceRate: { type: Number, required: true },
+    dopiste: { type: Number, required: true },
     condition: { type: String, enum: ['new', 'good', 'old'], required: true },
     type: { type: String, enum: ['home appliances', 'electronics', 'other'], required: true },
     owner: {
@@ -15,6 +16,7 @@ const itemSchema = mongoose.Schema({
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User'
     },
+    borrowDate: { type: Date },
     isAvailable: { type: Boolean, default: true, required: true }
 }, { timetamps: true })
 
