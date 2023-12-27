5187c167cbf68e04f0c51298d4f13d0f7ad400c1
anas029 <ali.yaqoob@gmail.com>
2023-12-27 21:31:54 +0300

    add logger function

--- config/db.js
+++ config/db.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
+const logger = require('../lib/logger')
 const db = process.env.DB_URI
 mongoose.set('strictQuery', true)
 mongoose.connect(db)
-    .then(() => console.log(`Connected to MongoDB database named: ${mongoose.connection.name}.`))
-    .catch((error) => console.error("Error: MongoDB is not connected. " + error.message));
+    .then(() => logger.info(`Connected to MongoDB database named: ${mongoose.connection.name}.`))
+    .catch((error) => logger.error("Error: MongoDB is not connected. " + error.message));
